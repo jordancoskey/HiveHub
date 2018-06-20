a86670524390013ae3ffe1a347ca132fa93a7816
Jordan Coskey <mini13coskey@gmail.com>
2018-06-20 14:37:24 +0200

    Added working files

---
+++ HiveHub/Controllers/ManagingAgencyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HiveHub.Controllers
+{
+    public class ManagingAgencyController : Controller
+    {
+        // GET: ManagingAgency
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HiveHub/Scripts/customScripts/checkboxClassChanger.js
@@ -0,0 +1,6 @@
+﻿$('.checkbox').click(function () {
+    $(this).toggleClass('checkbox fa fa-square-o');
+    $(this).toggleClass('checkbox fa fa-check-square');
+    $('.todo-item').toggleClass('todo-item done');
+    $('.todo-item done').toggleClass('todo-item');
+});
